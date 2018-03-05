beab1863c948d28df9f210c71c685014f7fbeef3
unknown <r.shiatu810@gmail.com>
2018-03-05 21:26:07 +0900

    FirstCommit

---
+++ hellolaravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!--resources/views/layouts/app.blade.php-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+
+        <!-- CSSとJavaScript -->
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- ナビバーの内容 -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
+<!--
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *-->
+
